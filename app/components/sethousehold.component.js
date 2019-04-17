d0c5446bd529fb01c75da4d7f6b49d8173b5bdf8
Justin Little <justinlittle1234@gmail.com>
2019-04-17 17:51:12 -0400

    added semi-functionality to setHousehold

--- app/components/sethousehold.component.js
+++ app/components/sethousehold.component.js
@@ -1,10 +1,54 @@
 import React, {Component} from 'react'
+import {
+  Button,
+  Item,
+  Input,
+  Icon,
+  Text,
+  Form
+} from 'native-base';
+import { observer } from 'mobx-react/native';
+import { observable } from 'mobx';
+import firebase from 'firebase';
 
+@observer
 export default class SetHousehold extends Component {
+  @observable housePIN = '';
+
   constructor(props) {
     super(props);
   }
+
+  setHouse() {
+    const { auth } = this.props.stores
+    const { navigate } = this.props.navigation
+
+
+    //var userId = firebase.auth().currentUser.userId
+    var ref = firebase.database().ref('/households/');
+    console.log(ref.key);
+  }
+
   render() {
-    return null;
+    const { auth } = this.props.stores
+    const { navigate } = this.props.navigation
+    return (
+      <Form>
+        <Item style={{marginBottom: 10}} rounded>
+          <Input style={{color: "#fff"}}
+            placeholder='5 digit House PIN'
+            placeholderTextColor="#fff"
+            onChangeText={(housePIN) => this.housePIN = housePIN}/>
+        </Item>
+        <Button rounded block style={{marginBottom: 10}}
+          onPress={this.setHouse.bind(this)}>
+          <Text>Set Household</Text>
+        </Button>
+        <Button rounded block style={{marginBottom: 10}}
+          onPress={() => navigate('CreateHousehold')}>
+          <Text>Create New Household</Text>
+        </Button>
+      </Form>
+    )
   }
 }
--- app/screens/sethousehold.screen.js
+++ app/screens/sethousehold.screen.js
@@ -1,27 +1,57 @@
 import React, {Component} from 'react';
-import {Text, StyleSheet} from 'react-native'
+import {Container, Content} from 'native-base';
+import {
+  View,
+  ImageBackground,
+  StyleSheet,
+  Dimensions
+} from 'react-native';
+import { inject } from 'mobx-react';
 
 import SetHousehold from '../components/sethousehold.component';
 
+@inject("stores")
 export default class SetHouseholdScreen extends Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      titleText: "Set Up Household"
-    };
+    super(props)
   }
   render() {
+    const {stores} = this.props
     return (
-      <Text style={styles.titleText}>
-        {this.state.titleText}
-      </Text>
-    );
+      <Container>
+        <View style={styles.container}>
+          <Content scrollEnabled={false}>
+            <ImageBackground style={styles.loginBackground} source={stores.config.loginBG}>
+              <View style={styles.loginForeground}>
+                <SetHousehold {...this.props}/>
+              </View>
+            </ImageBackground>
+          </Content>
+        </View>
+      </Container>
+    )
   }
 }
 
 const styles = StyleSheet.create({
-  titleText: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  container: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0
+  },
+  loginBackground: {
+    flex: 1,
+    width: null,
+    height: null
+  },
+  loginForeground: {
+    flex: 1,
+    marginTop: Dimensions.get('window').height/1.75,
+    paddingTop: 20,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 90,
+    bottom: 0
   }
-});
+})
